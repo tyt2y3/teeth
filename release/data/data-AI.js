a2297df30ff13d511d6e468353c6f366fd6b0301
Chris Tsang <tyt2y3@gmail.com>
2013-01-08 14:48:46 +0800

    fixing dangling commas

--- release/data/data-AI.js
+++ release/data/data-AI.js
@@ -2,6 +2,6 @@ define({
 	normal:
 	{
 		alert_distance: 6, //the distance to be alert of opponent to change state to chase
-		out_of_wander: 0.2, //probability to walk off the patrol path in wandering
+		out_of_wander: 0.2 //probability to walk off the patrol path in wandering
 	}
 });
